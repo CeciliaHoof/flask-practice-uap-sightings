967fa53ea4468663820d2f9450895f6104e0cad1
Cecilia <ccchimento@gmail.com>
2024-02-21 10:26:59 -0700

    completed dq

--- server/app.py
+++ server/app.py
@@ -4,16 +4,48 @@ from flask import Flask, make_response, request
 from flask_migrate import Migrate
 
 # import model and db instance
+from models import Sighting, db
 
 # Initialize Flask app
+app = Flask(__name__)
 
 # configure the app
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
 
 # Initialize Flask-Migrate
+migrate = Migrate(app, db)
 
 # Initialize the db instance
+db.init_app(app)
 
 # Define routes and views
+@app.route('/')
+def index():
+    response_body = {'message': "The UAPID welcome our new extraterrestrial overlords!"}
+    response = make_response(response_body, 200)
+    return response
+
+@app.route('/sightings')
+def sightings():
+    sightings = []
+    for sighting in Sighting.query.all():
+        sightings.append(sighting.to_dict())
+    body = {'sightings': sightings}
+    return make_response(body, 200)
+
+@app.route('/sightings/<int:id>')
+def sighting_by_id(id):
+    sighting = Sighting.query.filter(Sighting.id == id).first()
+    body = {'sighting': sighting.to_dict()}
+    return make_response(body, 200)
+
+@app.route('/sightings/<string:location>')
+def sighting_by_location(location):
+    sighting = Sighting.query.filter(Sighting.location == location).first()
+    body = {'sighting': sighting.to_dict()}
+    return make_response(body, 200)
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,66 @@
 # import app
+from app import app
 # import model and db instance
+from models import db, Sighting
+from faker import Faker
 
 # Define seeding functions (optional: use Faker to help generate fake data)
+def clear_database():
+    print('clearing db...')
+    Sighting.query.delete()
+
+def create_sightings():
+    print('creating sightings...')
+    sightings = []
+    fake = Faker()
+
+    s1 = Sighting(
+        date=fake.date(),
+        time=fake.time(),
+        location=fake.city(),
+        shape_of_craft=fake.word(),
+        approximate_size=fake.random_int(min=1, max=100),
+        approximate_speed=fake.random_int(min=1, max=100),
+        description=fake.sentence(),
+        reporter=fake.name(),
+        reporter_sober=fake.random_int(min=0, max=1)
+    )
+    sightings.append(s1)
+
+    s2 = Sighting(
+        date=fake.date(),
+        time=fake.time(),
+        location=fake.city(),
+        shape_of_craft=fake.word(),
+        approximate_size=fake.random_int(min=1, max=100),
+        approximate_speed=fake.random_int(min=1, max=100),
+        description=fake.sentence(),
+        reporter=fake.name(),
+        reporter_sober=fake.random_int(min=0, max=1)
+    )
+    sightings.append(s2)
+
+    s3 = Sighting(
+        date=fake.date(),
+        time=fake.time(),
+        location=fake.city(),
+        shape_of_craft=fake.word(),
+        approximate_size=fake.random_int(min=1, max=100),
+        approximate_speed=fake.random_int(min=1, max=100),
+        description=fake.sentence(),
+        reporter=fake.name(),
+        reporter_sober=fake.random_int(min=0, max=1)
+    )
+    sightings.append(s3)
+
+    db.session.add_all(sightings)
+    print('adding sightings to db...')
+
+    db.session.commit()
+
 
 if __name__ == "__main__":
     with app.app_context():
-        # Call seeding functions
+        clear_database()
+        create_sightings()
         pass
